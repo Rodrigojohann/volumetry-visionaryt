6973ae1c9d5af23e51b82afe0f2e78b04cbe93fc
Rodrigo Johann <Rodrigojohann@github.com>
2021-04-13 00:28:56 -0300

    adding to the calculation function

--- SampleVisionaryT/SampleVisionaryT.cpp
+++ SampleVisionaryT/SampleVisionaryT.cpp
@@ -32,16 +32,47 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/surface/convex_hull.h>
 
-//void calculatevolume(std::vector<PointXYZ> pointCloud)
-//{
-	////std::cout << 'rows: ' << distanceMap.size << '\n\n';
-	//printf("hi! \n\n");
-	////printf("columns: %d \n\n", pointCloud[100]);
-	//for(int i=0; i < pointCloud.size(); i++) printf("%d \n", pointCloud[i].z);
-	
-	
-	////for (std::vector<uint16_t>::const_iterator i = distanceMap.begin(); i != distanceMap.end(); ++i) std::cout << *i << ' ';
-//}
+void calculatevolume(std::vector<PointXYZ> inputcloud)
+{
+//maybe I have to convert input point cloud into a pcl format
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+cloud->points.resize (inputcloud.size());
+
+for(size_t i=0;i<cloud->points.size();++i)
+{
+	cloud->points[i].x = inputcloud[i].x;
+	cloud->points[i].y = inputcloud[i].y;
+	cloud->points[i].z = inputcloud[i].z;
+}
+
+
+size_t original_size = cloud->size();
+printf("original size: %d \n", original_size);
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+
+pcl::PassThrough<pcl::PointXYZ> pass;
+pass.setInputCloud (cloud);
+pass.setFilterFieldName ("z");
+pass.setFilterLimits (-5000, 5000);
+pass.filter (*cloud_filtered);
+size_t filtered_size = cloud_filtered->size();
+printf("pointcloud after filtering size: %d \n", filtered_size);
+
+pcl::ConvexHull<pcl::PointXYZ> chull;
+chull.setInputCloud(cloud_filtered);
+chull.setDimension(3);
+chull.setComputeAreaVolume(true);
+
+std::vector<pcl::Vertices> polygons;
+pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull(new pcl::PointCloud<pcl::PointXYZ>);
+chull.reconstruct(*surface_hull, polygons);
+
+double volume= chull.getTotalVolume();
+
+}
 
 
 
@@ -143,7 +174,7 @@ bool runStreamingDemo(char* ipAddress, unsigned short port)
     
 	//calculatevolume(pointCloud);
 
-	char* plyFilePath = "/home/rodrigo/Volumetry/volumetry-visionaryt/testcloud.ply";
+	char* plyFilePath = "/home/rodrigo/Volumetry/volumetry-visionaryt/testcloud_1.ply";
     printf("Writing frame to %s\n", plyFilePath);
     PointCloudPlyWriter::WriteFormatPLY(plyFilePath, pointCloud, true);
     printf("Finished writing frame to %s\n", plyFilePath);
