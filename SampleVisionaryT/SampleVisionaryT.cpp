d681319a550cd1465a6d6677c5725e3e65e244c7
Rodrigo Johann <Rodrigojohann@github.com>
2021-04-22 23:31:55 -0300

    proper indentation

--- SampleVisionaryT/SampleVisionaryT.cpp
+++ SampleVisionaryT/SampleVisionaryT.cpp
@@ -36,166 +36,164 @@
 
 void calculatevolume(std::vector<PointXYZ> inputcloud)
 {
-//maybe I have to convert input point cloud into a pcl format
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+	cloud->points.resize (inputcloud.size());
 
-cloud->points.resize (inputcloud.size());
+	for(size_t i=0;i<cloud->points.size();++i)
+	{
+		cloud->points[i].x = inputcloud[i].x;
+		cloud->points[i].y = inputcloud[i].y;
+		cloud->points[i].z = inputcloud[i].z;
+	}
 
-for(size_t i=0;i<cloud->points.size();++i)
-{
-	cloud->points[i].x = inputcloud[i].x;
-	cloud->points[i].y = inputcloud[i].y;
-	cloud->points[i].z = inputcloud[i].z;
-}
+	//pcl::io::savePLYFileASCII ("testcloud_pcl.ply", *cloud);
 
-//pcl::io::savePLYFileASCII ("testcloud_pcl.ply", *cloud);
+	size_t original_size = cloud->size();
+	printf("original size: %d \n", original_size);
 
-size_t original_size = cloud->size();
-printf("original size: %d \n", original_size);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PassThrough<pcl::PointXYZ> pass;
+	pass.setInputCloud (cloud);
+	pass.setFilterFieldName ("z");
+	pass.setFilterLimits (-5000, 5000);
+	pass.filter (*cloud_filtered);
+	size_t filtered_size = cloud_filtered->size();
+	printf("pointcloud after filtering size: %d \n", filtered_size);
 
-pcl::PassThrough<pcl::PointXYZ> pass;
-pass.setInputCloud (cloud);
-pass.setFilterFieldName ("z");
-pass.setFilterLimits (-5000, 5000);
-pass.filter (*cloud_filtered);
-size_t filtered_size = cloud_filtered->size();
-printf("pointcloud after filtering size: %d \n", filtered_size);
+	pcl::ConvexHull<pcl::PointXYZ> chull;
+	chull.setInputCloud(cloud_filtered);
+	chull.setDimension(3);
+	chull.setComputeAreaVolume(true);
 
-pcl::ConvexHull<pcl::PointXYZ> chull;
-chull.setInputCloud(cloud_filtered);
-chull.setDimension(3);
-chull.setComputeAreaVolume(true);
+	std::vector<pcl::Vertices> polygons;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull(new pcl::PointCloud<pcl::PointXYZ>);
+	chull.reconstruct(*surface_hull, polygons);
 
-std::vector<pcl::Vertices> polygons;
-pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull(new pcl::PointCloud<pcl::PointXYZ>);
-chull.reconstruct(*surface_hull, polygons);
-
-double volume= chull.getTotalVolume();
-printf("volume: %f \n\n", volume);
+	double volume= chull.getTotalVolume();
+	printf("volume: %f \n\n", volume);
 }
 
 
 
 bool runStreamingDemo(char* ipAddress, unsigned short port)
 {
-  // Generate Visionary instance
-  boost::shared_ptr<VisionaryTData> pDataHandler = boost::make_shared<VisionaryTData>();
-  VisionaryDataStream dataStream(pDataHandler, inet_addr(ipAddress), htons(port));
-  VisionaryControl control(inet_addr(ipAddress), htons(2112));
+	// Generate Visionary instance
+	boost::shared_ptr<VisionaryTData> pDataHandler = boost::make_shared<VisionaryTData>();
+	VisionaryDataStream dataStream(pDataHandler, inet_addr(ipAddress), htons(port));
+	VisionaryControl control(inet_addr(ipAddress), htons(2112));
   
-  //-----------------------------------------------
-  // Connect to devices data stream 
-  if (!dataStream.openConnection())
-  {
-    printf("Failed to open data stream connection to device.\n");
-    return false;   // connection failed
-  }
+	//-----------------------------------------------
+	// Connect to devices data stream 
+	if (!dataStream.openConnection())
+	{
+		printf("Failed to open data stream connection to device.\n");
+		return false;   // connection failed
+	}
 
-  //-----------------------------------------------
-  // Connect to devices control channel
-  if (!control.openConnection())
-  {
-    printf("Failed to open control connection to device.\n");
-    return false;   // connection failed
-  }
+	//-----------------------------------------------
+	// Connect to devices control channel
+	if (!control.openConnection())
+	{
+		printf("Failed to open control connection to device.\n");
+		return false;   // connection failed
+	}
 
-  //-----------------------------------------------
-  // Login as authorized client
-  if (control.login(CoLaUserLevel::AUTHORIZED_CLIENT, "CLIENT"))
-  {
-    //-----------------------------------------------
-    // An example of reading an writing device parameters is shown here.
-    // Use the "SOPAS Communication Interface Description" PDF to determine data types for other variables
+	//-----------------------------------------------
+	// Login as authorized client
+	if (control.login(CoLaUserLevel::AUTHORIZED_CLIENT, "CLIENT"))
+	{
+		//-----------------------------------------------
+		// An example of reading an writing device parameters is shown here.
+		// Use the "SOPAS Communication Interface Description" PDF to determine data types for other variables
 
-    //-----------------------------------------------
-    // Set integrationTimeUs parameter to 150000
-    printf("Setting integrationTimeUs to 3800\n");
-    CoLaBCommand setIngrationTimeCommand = CoLaBCommandBuilder(CoLaCommandType::WRITE_VARIABLE, "integrationTimeUs").parameterUDInt(3800).build();
-    CoLaBCommand setIngrationTimeResponse = control.sendCommand(setIngrationTimeCommand);
+		//-----------------------------------------------
+		// Set integrationTimeUs parameter to 150000
+		printf("Setting integrationTimeUs to 3800\n");
+		CoLaBCommand setIngrationTimeCommand = CoLaBCommandBuilder(CoLaCommandType::WRITE_VARIABLE, "integrationTimeUs").parameterUDInt(3800).build();
+		CoLaBCommand setIngrationTimeResponse = control.sendCommand(setIngrationTimeCommand);
 
-    //-----------------------------------------------
-    // Read integrationTimeUs parameter
-    CoLaBCommand getIntegrationTimeCommand = CoLaBCommandBuilder(CoLaCommandType::READ_VARIABLE, "integrationTimeUs").build();
-    CoLaBCommand integrationTimeResponse = control.sendCommand(getIntegrationTimeCommand);
-    uint32_t integrationTimeUs = CoLaBCommandReader(integrationTimeResponse).readUDInt();
-    printf("Read integrationTimeUs = %d\n", integrationTimeUs);
+		//-----------------------------------------------
+		// Read integrationTimeUs parameter
+		CoLaBCommand getIntegrationTimeCommand = CoLaBCommandBuilder(CoLaCommandType::READ_VARIABLE, "integrationTimeUs").build();
+		CoLaBCommand integrationTimeResponse = control.sendCommand(getIntegrationTimeCommand);
+		uint32_t integrationTimeUs = CoLaBCommandReader(integrationTimeResponse).readUDInt();
+		printf("Read integrationTimeUs = %d\n", integrationTimeUs);
 
-    //-----------------------------------------------
-    // Read info messages variable
-    CoLaBCommand getMessagesCommand = CoLaBCommandBuilder(CoLaCommandType::READ_VARIABLE, "MSinfo").build();
-    CoLaBCommand messagesResponse = control.sendCommand(getMessagesCommand);
+		//-----------------------------------------------
+		// Read info messages variable
+		CoLaBCommand getMessagesCommand = CoLaBCommandBuilder(CoLaCommandType::READ_VARIABLE, "MSinfo").build();
+		CoLaBCommand messagesResponse = control.sendCommand(getMessagesCommand);
 
-    //-----------------------------------------------
-    // Read message array, length of array is always 25 items (see PDF).
-    CoLaBCommandReader reader(messagesResponse);
-    for (int i = 0; i < 25; i++)
-    {
-      uint32_t errorId = reader.readUDInt();
-      uint32_t errorState = reader.readUDInt();
-      uint16_t firstTimePwrOnCount = reader.readUInt();
-      uint32_t firstTimeOpSecs = reader.readUDInt();
-      uint32_t firstTimeTimeOccur = reader.readUDInt();
-      uint16_t lastTimePwrOnCount = reader.readUInt();
-      uint32_t lastTimeOpSecs = reader.readUDInt();
-      uint32_t lastTimeTimeOccur = reader.readUDInt();
-      uint16_t numberOccurance = reader.readUInt();
-      uint16_t errReserved = reader.readUInt();
-      std::string extInfo = reader.readFlexString();
+		//-----------------------------------------------
+		// Read message array, length of array is always 25 items (see PDF).
+		CoLaBCommandReader reader(messagesResponse);
+		for (int i = 0; i < 25; i++)
+		{
+			uint32_t errorId = reader.readUDInt();
+			uint32_t errorState = reader.readUDInt();
+			uint16_t firstTimePwrOnCount = reader.readUInt();
+			uint32_t firstTimeOpSecs = reader.readUDInt();
+			uint32_t firstTimeTimeOccur = reader.readUDInt();
+			uint16_t lastTimePwrOnCount = reader.readUInt();
+			uint32_t lastTimeOpSecs = reader.readUDInt();
+			uint32_t lastTimeTimeOccur = reader.readUDInt();
+			uint16_t numberOccurance = reader.readUInt();
+			uint16_t errReserved = reader.readUInt();
+			std::string extInfo = reader.readFlexString();
 
-      if (errorId != 0)
-      {
-        printf("Info message [0x%032x], extInfo: %s, numberOccurance: %d\n", errorId, extInfo.c_str(), numberOccurance);
-      }
-    }
-  }
+			if (errorId != 0)
+			{
+				printf("Info message [0x%032x], extInfo: %s, numberOccurance: %d\n", errorId, extInfo.c_str(), numberOccurance);
+			}
+		}
+	}
 
-  //-----------------------------------------------
-  // Logout from device after reading variables.
-  if (!control.logout())
-  {
-    printf("Failed to logout\n");
-  }
+	//-----------------------------------------------
+	// Logout from device after reading variables.
+	if (!control.logout())
+	{
+		printf("Failed to logout\n");
+	}
 
-  //-----------------------------------------------
-  // Stop image acquisition (works always, also when already stopped)
-  control.stopAcquisition();
+	//-----------------------------------------------
+	// Stop image acquisition (works always, also when already stopped)
+	control.stopAcquisition();
 
-  //-----------------------------------------------
-  // Capture a single image
-  control.stepAcquisition();
-  if (dataStream.getNextFrame())
-  {
-    printf("Frame received through step called, frame #%d, timestamp: %I64u \n", pDataHandler->getFrameNum(), pDataHandler->getTimestampMS());
+	//-----------------------------------------------
+	// Capture a single image
+	control.stepAcquisition();
+	if (dataStream.getNextFrame())
+	{
+		printf("Frame received through step called, frame #%d, timestamp: %I64u \n", pDataHandler->getFrameNum(), pDataHandler->getTimestampMS());
 
-    //-----------------------------------------------
-    // Convert data to a point cloud
-    std::vector<PointXYZ> pointCloud;
-    pDataHandler->generatePointCloud(pointCloud);
-    
-	calculatevolume(pointCloud);
+		//-----------------------------------------------
+		// Convert data to a point cloud
+		std::vector<PointXYZ> pointCloud;
+		pDataHandler->generatePointCloud(pointCloud);
 
-	//char* plyFilePath = "/home/rodrigo/Volumetry/volumetry-visionaryt/testcloud_original.ply";
-    //printf("Writing frame to %s\n", plyFilePath);
-    //PointCloudPlyWriter::WriteFormatPLY(plyFilePath, pointCloud, true);
-    //printf("Finished writing frame to %s\n", plyFilePath);
+		calculatevolume(pointCloud);
+
+		//char* plyFilePath = "/home/rodrigo/Volumetry/volumetry-visionaryt/testcloud_original.ply";
+		//printf("Writing frame to %s\n", plyFilePath);
+		//PointCloudPlyWriter::WriteFormatPLY(plyFilePath, pointCloud, true);
+		//printf("Finished writing frame to %s\n", plyFilePath);
 
 
-  control.stopAcquisition();
-  control.closeConnection();
-  dataStream.closeConnection();
-  return true;
-  }
+		  control.stopAcquisition();
+		  control.closeConnection();
+		  dataStream.closeConnection();
+		  return true;
+	}
 }
 
 
 int main()
 {
-  // Insert IP of your camera and the API port
-  /// Default values:
-  /// IP:        "192.168.1.10"
-  /// API-port:  2114
-  runStreamingDemo("192.168.15.40", 2114);
+	// Insert IP of your camera and the API port
+	/// Default values:
+	/// IP:        "192.168.1.10"
+	/// API-port:  2114
+	runStreamingDemo("192.168.15.40", 2114);
 }
