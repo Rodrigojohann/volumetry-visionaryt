055420a8448191a636e962cec7182280a761177a
Rodrigo Johann <Rodrigojohann@github.com>
2021-05-11 22:51:10 -0300

    cleaned code

--- SampleVisionaryT/SampleVisionaryT.cpp
+++ SampleVisionaryT/SampleVisionaryT.cpp
@@ -1,20 +1,4 @@
-//
-// Copyright note: Redistribution and use in source, with or without modification, are permitted.
-// 
-// Created: November 2016
-// 
-// @author:  Andreas Richert
-// SICK AG, Waldkirch
-// email: TechSupport0905@sick.de
-// 
-// Last commit: $Date: 2018-02-15 13:51:24 +0100 (Do, 15 Feb 2018) $
-// Last editor: $Author: richean $
-// 
-// Version "$Revision: 16012 $"
-//
-
 #include <stdio.h>
-#include <iostream>
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
 #include "VisionaryTData.h"    // Header specific for the Time of Flight data
@@ -22,9 +6,6 @@
 #include "VisionaryControl.h"
 #include "PointXYZ.h"
 #include "PointCloudPlyWriter.h"
-#include "CoLaBCommandBuilder.h"
-#include "CoLaBCommandReader.h"
-#include <vector>
 #include <pcl/point_types.h>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/sample_consensus/method_types.h>
@@ -32,11 +13,8 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/surface/convex_hull.h>
 
-#include <pcl/io/ply_io.h>
-
 void calculatevolume(std::vector<PointXYZ> inputcloud)
 {
-
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
 	cloud->points.resize (inputcloud.size());
 
@@ -50,32 +28,16 @@ void calculatevolume(std::vector<PointXYZ> inputcloud)
 	size_t original_size = cloud->size();
 	printf("original size: %d \n", original_size);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_x (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_y (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_z (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
 	pcl::PassThrough<pcl::PointXYZ> passx;
 	passx.setInputCloud (cloud);
 	passx.setFilterFieldName ("x");
-	passx.setFilterLimits (-0.250, 0.250);
-	passx.filter (*cloud_filtered_x);
+	passx.setFilterLimits (-5000, 5000);
+	passx.filter (*cloud_filtered);
 	
-	pcl::PassThrough<pcl::PointXYZ> passy;
-	passy.setInputCloud (cloud_filtered_x);
-	passy.setFilterFieldName ("y");
-	passy.setFilterLimits (0, 0.819);
-	passy.filter (*cloud_filtered_y);
-	
-	pcl::PassThrough<pcl::PointXYZ> passz;
-	passz.setInputCloud (cloud_filtered_y);
-	passz.setFilterFieldName ("z");
-	passz.setFilterLimits (-0.810, -0.400);
-	passz.filter (*cloud_filtered_z);
-	size_t filtered_size = cloud_filtered_z->size();
-	printf("pointcloud after filtering size: %d \n", filtered_size);
-
 	pcl::ConvexHull<pcl::PointXYZ> chull;
-	chull.setInputCloud(cloud_filtered_z);
+	chull.setInputCloud(cloud_filtered);
 	chull.setDimension(3);
 	chull.setComputeAreaVolume(true);
 
@@ -83,11 +45,11 @@ void calculatevolume(std::vector<PointXYZ> inputcloud)
 	pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull(new pcl::PointCloud<pcl::PointXYZ>);
 	chull.reconstruct(*surface_hull, polygons);
 
-	double volume= chull.getTotalVolume();
-	printf("volume: %f \n\n", volume);
+	double volume = chull.getTotalVolume();
+	double volumecm = volume*1000000;
+	printf("volume: %f m³\n\n", volume);
+	printf("volume: %f cm³\n\n", volumecm);
 }
-
-
 
 bool runStreamingDemo(char* ipAddress, unsigned short port)
 {
@@ -112,25 +74,17 @@ bool runStreamingDemo(char* ipAddress, unsigned short port)
 		return false;   // connection failed
 	}
 
-	//-----------------------------------------------
-	// Stop image acquisition (works always, also when already stopped)
 	control.stopAcquisition();
-
-	//-----------------------------------------------
-	// Capture a single image
-	//control.stepAcquisition();
 	control.startAcquisition();
 	for (int i = 0; i < 10; i++)
 	{
 		if (dataStream.getNextFrame())
 		{
-			printf("Frame received through step called, frame #%d, timestamp: %I64u \n", pDataHandler->getFrameNum(), pDataHandler->getTimestampMS());
-
-			//-----------------------------------------------
 			// Convert data to a point cloud
 			std::vector<PointXYZ> pointCloud;
 			pDataHandler->generatePointCloud(pointCloud);
 
+			// Calculate volume
 			calculatevolume(pointCloud);
 			
 			if (i == 9)
@@ -148,7 +102,6 @@ bool runStreamingDemo(char* ipAddress, unsigned short port)
 	dataStream.closeConnection();
 	return true;
 }
-
 
 int main()
 {
