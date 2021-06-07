8dc444bfae254172807c91d023c457627a2d614d
Rodrigo Johann <Rodrigojohann@github.com>
2021-06-06 23:03:39 -0300

    optimizing

--- SampleVisionaryT/SampleVisionaryT.cpp
+++ SampleVisionaryT/SampleVisionaryT.cpp
@@ -13,9 +13,20 @@
 #include <pcl/surface/convex_hull.h>
 #include <pcl/filters/passthrough.h>
 
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+size_t original_size;
+pcl::PassThrough<pcl::PointXYZ> passx;
+pcl::ConvexHull<pcl::PointXYZ> chull;
+std::vector<pcl::Vertices> polygons;
+pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull(new pcl::PointCloud<pcl::PointXYZ>);
+double volume;
+double volumecm;
+std::vector<PointXYZ> pointCloud;
+boost::shared_ptr<VisionaryTData> pDataHandler;
+
 void calculatevolume(std::vector<PointXYZ> inputcloud)
 {
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
 	cloud->points.resize (inputcloud.size());
 
 	for(size_t i=0;i<cloud->points.size();++i)
@@ -25,28 +36,21 @@ void calculatevolume(std::vector<PointXYZ> inputcloud)
 		cloud->points[i].z = inputcloud[i].z;
 	}
 
-	size_t original_size = cloud->size();
+	original_size = cloud->size();
 	printf("original size: %d \n", original_size);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-
-	pcl::PassThrough<pcl::PointXYZ> passx;
 	passx.setInputCloud (cloud);
 	passx.setFilterFieldName ("x");
 	passx.setFilterLimits (-5000, 5000);
 	passx.filter (*cloud_filtered);
 
-	pcl::ConvexHull<pcl::PointXYZ> chull;
 	chull.setInputCloud(cloud_filtered);
 	chull.setDimension(3);
 	chull.setComputeAreaVolume(true);
-
-	std::vector<pcl::Vertices> polygons;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull(new pcl::PointCloud<pcl::PointXYZ>);
 	chull.reconstruct(*surface_hull, polygons);
 
-	double volume = chull.getTotalVolume();
-	double volumecm = volume*1000000;
+	volume = chull.getTotalVolume();
+	volumecm = volume*1000000;
 	printf("volume: %f m³\n\n", volume);
 	printf("volume: %f cm³\n\n", volumecm);
 }
@@ -54,10 +58,9 @@ void calculatevolume(std::vector<PointXYZ> inputcloud)
 bool runStreamingDemo(char* ipAddress, unsigned short port)
 {
 	// Generate Visionary instance
-	boost::shared_ptr<VisionaryTData> pDataHandler = boost::make_shared<VisionaryTData>();
+	pDataHandler = boost::make_shared<VisionaryTData>();
 	VisionaryDataStream dataStream(pDataHandler, inet_addr(ipAddress), htons(port));
 	VisionaryControl control(inet_addr(ipAddress), htons(2112));
-  
 	//-----------------------------------------------
 	// Connect to devices data stream 
 	if (!dataStream.openConnection())
@@ -65,7 +68,6 @@ bool runStreamingDemo(char* ipAddress, unsigned short port)
 		printf("Failed to open data stream connection to device.\n");
 		return false;   // connection failed
 	}
-
 	//-----------------------------------------------
 	// Connect to devices control channel
 	if (!control.openConnection())
@@ -73,30 +75,22 @@ bool runStreamingDemo(char* ipAddress, unsigned short port)
 		printf("Failed to open control connection to device.\n");
 		return false;   // connection failed
 	}
-
 	control.stopAcquisition();
 	control.startAcquisition();
-	for (int i = 0; i < 10; i++)
+	while (true)
 	{
 		if (dataStream.getNextFrame())
 		{
 			// Convert data to a point cloud
-			std::vector<PointXYZ> pointCloud;
 			pDataHandler->generatePointCloud(pointCloud);
-
 			// Calculate volume
 			calculatevolume(pointCloud);
-			
-			if (i == 9)
-			{
-				char* plyFilePath = "/home/rodrigo/Volumetry/volumetry-visionaryt/testcloud_original.ply";
-				printf("Writing frame to %s\n", plyFilePath);
-				PointCloudPlyWriter::WriteFormatPLY(plyFilePath, pointCloud, true);
-				printf("Finished writing frame to %s\n", plyFilePath);
-			}
 		}
+		if (getchar() == 'q')
+		{
+			break;
+		}	
 	}
-
 	control.stopAcquisition();
 	control.closeConnection();
 	dataStream.closeConnection();
@@ -108,5 +102,5 @@ int main()
 	/// Default values:
 	/// IP:        "192.168.1.10"
 	/// API-port:  2114
-	runStreamingDemo("192.168.15.40", 2114);
+	runStreamingDemo((char*)"192.168.15.40", 2114);
 }
