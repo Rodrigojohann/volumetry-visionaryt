b04a2112b1bf964f40875f27f5945bd9646d94ae
Rodrigo Johann <Rodrigojohann@github.com>
2021-06-15 23:29:47 -0300

    filter added

--- volume-visionaryt.cpp
+++ volume-visionaryt.cpp
@@ -17,23 +17,24 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/surface/convex_hull.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-size_t original_size;
 pcl::PassThrough<pcl::PointXYZ> passx;
 pcl::PassThrough<pcl::PointXYZ> passy;
 pcl::PassThrough<pcl::PointXYZ> passz;
 pcl::ConvexHull<pcl::PointXYZ> chull;
-std::vector<pcl::Vertices> polygons;
 pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+std::vector<pcl::Vertices> polygons;
 double volume;
-double volumecm;
 double volumemean;
 std::vector<PointXYZ> pointCloud;
 boost::shared_ptr<VisionaryTData> pDataHandler;
-int i;
+int counter;
+
 
 bool kbhit(void)
 {
@@ -61,9 +62,6 @@ double calculatevolume(std::vector<PointXYZ> inputcloud)
 		cloud->points[i].z = inputcloud[i].z;
 	}
 
-	original_size = cloud->size();
-	//printf("original size: %d \n", original_size);
-
 	passx.setInputCloud (cloud);
 	passx.setFilterFieldName ("x");
 	passx.setFilterLimits (-0.24, 0.2);
@@ -76,8 +74,13 @@ double calculatevolume(std::vector<PointXYZ> inputcloud)
 	
 	passz.setInputCloud (cloud_filtered);
 	passz.setFilterFieldName ("z");
-	passz.setFilterLimits (0, 0.80);
+	passz.setFilterLimits (0, 0.79);
 	passz.filter (*cloud_filtered);
+	
+	sor.setInputCloud (cloud_filtered);
+	sor.setMeanK (5);
+	sor.setStddevMulThresh (3.5);
+	sor.filter (*cloud_filtered);
 
 	chull.setInputCloud(cloud_filtered);
 	chull.setDimension(3);
@@ -85,9 +88,6 @@ double calculatevolume(std::vector<PointXYZ> inputcloud)
 	chull.reconstruct(*surface_hull, polygons);
 
 	volume = chull.getTotalVolume();
-	//volumecm = volume*1000000;
-	//printf("volume: %f m³\n\n", volume);
-	//printf("volume: %f cm³\n\n", volumecm);
 	return volume;
 }
 
@@ -115,7 +115,7 @@ void runStreamingDemo(char* ipAddress, unsigned short port)
 	volumemean = 0.0;
 	while (!kbhit())
 	{
-		i = i+1; 
+		counter = counter+1; 
 		if (dataStream.getNextFrame())
 		{
 			// Convert data to a point cloud
@@ -124,10 +124,11 @@ void runStreamingDemo(char* ipAddress, unsigned short port)
 			volumemean = volumemean + calculatevolume(pointCloud);
 		}
 
-		if (i==9)
+		if (counter==9)
 		{
-			i = 0;
+			counter = 0;
 			volumemean = volumemean/10;
+			printf("---------------------\n\n");
 			printf("volume: %f m³\n\n", volumemean);
 			printf("volume: %f cm³\n\n", volumemean*1000000);
 			volumemean = 0.0;			
