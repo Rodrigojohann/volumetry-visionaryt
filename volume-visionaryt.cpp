51d8fcb32c957513b8433fdd048c8d178ae857ec
rodrigo <rodrigo@debian.debian>
2021-08-04 10:09:59 -0300

    removing background

--- volume-visionaryt.cpp
+++ volume-visionaryt.cpp
@@ -109,8 +109,8 @@ void calculatevolume(std::vector<PointXYZ> inputcloud)
 	
 	for (size_t i = 0; i < newPointIdxVector.size(); ++i)
 	{
-		cloud_nobackground->points[i].x = cloud->points[newPointIdxVector[i]].x;
-		cloud_nobackground->points[i].y = cloud->points[newPointIdxVector[i]].y;
+		cloud_nobackground->points[i].x = cloud[newPointIdxVector[i]].x;
+		cloud_nobackground->points[i].y = cloud[newPointIdxVector[i]].y;
 		cloud_nobackground->points[i].z = cloud[newPointIdxVector[i]].z;
 	}
 }	
