9f81f00406ce67cfd1e46509ebb61954a182569c
rodrigo <rodrigo@debian.debian>
2021-08-04 13:39:47 -0300

    asdasdsa

--- compare-clouds.cpp
+++ compare-clouds.cpp
@@ -19,18 +19,30 @@
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_original (new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
-int main
+pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+
+int main()
 {
 	pcl::io::loadPLYFile<pcl::PointXYZ> ("volumetry-background/backgroundcloud.ply", *cloud_original);
-	pcl::io::loadPLYFile<pcl::PointXYZ> ("volumetry-background/backgroundcloud.ply", *cloud_filtered);
+	pcl::io::loadPLYFile<pcl::PointXYZ> ("volumetry-background/cloud_nobackground.ply", *cloud_filtered);
 
+	sor.setInputCloud (cloud_filtered);
+	sor.setMeanK (50);
+	sor.setStddevMulThresh (3.5);
+	sor.filter (*cloud_filtered);
+	
+	
 	// ---------------------- Visualizer -------------------------------------------
 	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer);
 	viewer->setBackgroundColor(0, 0, 0);
 
-	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler(cloud, 255, 0, 0);
-	viewer->addPointCloud(cloud_original, color_handler, "sample cloud");
-	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 6, "sample cloud");
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler1(cloud_original, 255, 0, 0);
+	viewer->addPointCloud(cloud_original, color_handler1, "sample cloud1");
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler2(cloud_filtered, 0, 0, 255);
+	viewer->addPointCloud(cloud_filtered, color_handler2, "sample cloud2");
+
+	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 6, "sample cloud1");
+	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 6, "sample cloud2");
 	while (!viewer->wasStopped())
 		{
 			viewer->spinOnce(100);
