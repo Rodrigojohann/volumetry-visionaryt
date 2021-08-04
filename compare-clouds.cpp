00ef4af589d7f550bf9d9cabdd33f21294944fb4
rodrigo <rodrigo@debian.debian>
2021-08-04 10:43:41 -0300

    compare-clouds file

---
+++ compare-clouds.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <termio.h>
+#include <unistd.h>
+#include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/surface/convex_hull.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/common/common.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/common/io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_original (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+
+
+
+int main
+{
+	pcl::io::loadPLYFile<pcl::PointXYZ> ("volumetry-background/backgroundcloud.ply", *cloud_original);
+	pcl::io::loadPLYFile<pcl::PointXYZ> ("volumetry-background/backgroundcloud.ply", *cloud_filtered);
+
+	// ---------------------- Visualizer -------------------------------------------
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer);
+	viewer->setBackgroundColor(0, 0, 0);
+
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler(cloud, 255, 0, 0);
+	viewer->addPointCloud(cloud_original, color_handler, "sample cloud");
+	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 6, "sample cloud");
+	while (!viewer->wasStopped())
+		{
+			viewer->spinOnce(100);
+		}
+
+}
