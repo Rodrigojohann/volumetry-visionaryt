36e755f40034bb63d9c14aca9dac2eda2204bb9c
rodrigo <rodrigo@debian.debian>
2021-08-04 08:40:00 -0300

    asdasdas

--- volume-visionaryt.cpp
+++ volume-visionaryt.cpp
@@ -25,7 +25,6 @@
 
 #include <pcl/common/io.h>
 #include <pcl/point_cloud.h>
-#include <pcl/octree/octree.h>
 #include <pcl/octree/octree_pointcloud_changedetector.h>
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
@@ -76,8 +75,7 @@ double calculatevolume(std::vector<PointXYZ> inputcloud)
 {
 	float resolution = 32.0f;
 	pcl::io::loadPLYFile<pcl::PointXYZ> ("volumetry-background/backgroundclound.ply", *cloud_background);
-	//pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ> octree (resolution);
-	Pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ>octree(resolution);
+	pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ> octree (resolution);
 	// Add points from cloudA to octree
 	//octree.setInputCloud (cloud_background);
 	//octree.addPointsFromInputCloud ();
