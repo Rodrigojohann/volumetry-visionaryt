04b80af3bbd69ef277b206d8598cb153d15bb4fe
rodrigo <rodrigo@debian.debian>
2021-08-17 10:23:17 -0300

    update

--- segmentation-test.cpp
+++ segmentation-test.cpp
@@ -12,7 +12,6 @@
 #include <pcl/segmentation/sac_segmentation.h>
 
 #include <stdio.h>
-#include <opencv2/opencv.hpp>
 
 #include <pcl/search/search.h>
 #include <pcl/search/kdtree.h>
@@ -21,8 +20,6 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/segmentation/region_growing.h>
 #include <pcl/features/moment_of_inertia_estimation.h>
-
-using namespace cv;
 
 pcl::PointXYZ                       minPt, maxPt;
 
@@ -42,8 +39,8 @@ double findMedian(double a[], int n)
 int main (int argc, char** argv)
 {
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::io::loadPCDFile <pcl::PointXYZ> ("volumetry-background/backgroundcloud5.pcd", *cloud);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::io::loadPCDFile <pcl::PointXYZ> ("volumetry-background/cloud-filtered.pcd", *cloud_filtered);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr outputcloud (new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr segmented_cloud (new pcl::PointCloud<pcl::PointXYZ>);
 
@@ -51,10 +48,10 @@ int main (int argc, char** argv)
 	   //<< " data points (" << pcl::getFieldsList (*cloud) << ")." << std::endl;
 
 	// Create the filtering object
-	pcl::VoxelGrid<pcl::PointXYZ> sor;
-	sor.setInputCloud (cloud);
-	sor.setLeafSize (0.01f, 0.01f, 0.01f);
-	sor.filter (*cloud_filtered);
+	//pcl::VoxelGrid<pcl::PointXYZ> sor;
+	//sor.setInputCloud (cloud);
+	//sor.setLeafSize (0.01f, 0.01f, 0.01f);
+	//sor.filter (*cloud_filtered);
 
 	float max_z, min_z;
 	pcl::getMinMax3D(*cloud_filtered, minPt, maxPt);
@@ -73,7 +70,7 @@ int main (int argc, char** argv)
 	pcl::PassThrough<pcl::PointXYZ> pass;
 	pass.setInputCloud (cloud_filtered);
 	pass.setFilterFieldName ("y");
-	pass.setFilterLimits (-0.35, 0.65);
+	pass.setFilterLimits (-0.55, 0.65);
 	pass.filter (*indices);
 
 	pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
@@ -96,14 +93,14 @@ int main (int argc, char** argv)
 	std::cout << "These are the indices of the points of the initial" <<
 	std::endl << "cloud that belong to the first cluster:" << std::endl;
 	
-	//pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
-	//pcl::visualization::CloudViewer viewer ("Cluster viewer");
-	//viewer.showCloud(colored_cloud);
-	//while (!viewer.wasStopped ())
-	//{
-	//}
+	pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
+	pcl::visualization::CloudViewer viewer ("Cluster viewer");
+	viewer.showCloud(colored_cloud);
+	while (!viewer.wasStopped ())
+	{
+	}
 
-	//return (0);
+	return (0);
 
 	int number = 4;
 	segmented_cloud->points.resize(clusters[number].indices.size());
@@ -158,23 +155,23 @@ int main (int argc, char** argv)
 	
 	//std::cout << normals;
 	
-	// ---------------------- Visualizer -------------------------------------------
-	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer);
-	viewer->setBackgroundColor(0, 0, 0);
+	//// ---------------------- Visualizer -------------------------------------------
+	//boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer);
+	//viewer->setBackgroundColor(0, 0, 0);
 
-	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler1(cloud, 255, 0, 0);
-	viewer->addPointCloud(cloud_filtered, color_handler1, "sample cloud1");
-	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler2(cloud, 0, 0, 255);
-	viewer->addPointCloud(segmented_cloud, color_handler2, "sample cloud2");
-	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 6, "sample cloud1");
-	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 6, "sample cloud2");
-	Eigen::Vector3f position (position_OBB.x, position_OBB.y, position_OBB.z);
-	Eigen::Quaternionf quat (rotational_matrix_OBB);
-	viewer->addCube (position, quat, max_point_OBB.x - min_point_OBB.x, max_point_OBB.y - min_point_OBB.y, max_point_OBB.z - min_point_OBB.z, "OBB");
-	viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "OBB");
-	while (!viewer->wasStopped())
-		{
-			viewer->spinOnce(100);
-		}
+	//pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler1(cloud, 255, 0, 0);
+	//viewer->addPointCloud(cloud_filtered, color_handler1, "sample cloud1");
+	//pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler2(cloud, 0, 0, 255);
+	//viewer->addPointCloud(segmented_cloud, color_handler2, "sample cloud2");
+	//viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 6, "sample cloud1");
+	//viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 6, "sample cloud2");
+	//Eigen::Vector3f position (position_OBB.x, position_OBB.y, position_OBB.z);
+	//Eigen::Quaternionf quat (rotational_matrix_OBB);
+	//viewer->addCube (position, quat, max_point_OBB.x - min_point_OBB.x, max_point_OBB.y - min_point_OBB.y, max_point_OBB.z - min_point_OBB.z, "OBB");
+	//viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "OBB");
+	//while (!viewer->wasStopped())
+		//{
+			//viewer->spinOnce(100);
+		//}
   	
 }
